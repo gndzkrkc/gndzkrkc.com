64dff88e2b1bbcd29bed43d1b030be9474c89341
Gündüz <3319717+gndzkrkc@users.noreply.github.com>
2025-12-11 21:27:38 +0300

    feat(i18n): Set localePrefix to 'always' for robust routing

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -3,7 +3,7 @@ import { defineRouting } from 'next-intl/routing';
 export const i18nRoutingConfig = defineRouting({
   locales: ['en', 'tr'],
   defaultLocale: 'en',
-  localePrefix: 'never',
+  localePrefix: 'always',
   pathnames: {
     '/': '/',
 
